c8c5fb8317e3f78234dbd381d75630ef1de958f2
Romain Lécart <rlecart@student.42.fr>
2024-01-01 22:55:57 +0100

    options to prod

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
 import options from "./options";
 
-export const API = `https://${options.back.path}:${options.back.port}`;
-// export const API = `https://${options.back.path}/api`;
+// export const API = `https://${options.back.path}:${options.back.port}`;
+export const API = `https://${options.back.path}/api`;
